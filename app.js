5ae6d2f0b2372ed67a151d02cc0b936a3ce17b7e
hungvi122 <caovi1325@gmail.com>
2016-10-08 12:05:15 +0700

    Init project

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+	nicknames = [];
+	server.listen(3000);
+	app.get('',function(req,res){
+		res.sendfile(__dirname + '/index.html');
+	});
+
+io.sockets.on('connection',function(socket){
+	socket.on('new user',function(data,callback){
+		if(nicknames.indexOf(data)!= -1){
+			callback(false);
+		}else{
+			callback(true);
+			socket.nickname= data;
+			nicknames.push(socket.nickname);
+			updateNickname();
+		}
+	});
+	function updateNickname(){
+		io.sockets.emit('usernames',nicknames);
+	}
+	socket.on('send message',function(data){
+		io.sockets.emit('new message', data);
+		socket.broadcast.emit('new message',data);
+	});
+	socket.on('disconnect', function(data){
+		if(!socket.nickname)return;
+		nicknames.splice(nicknames.indexOf(socket.nickname),1);
+		updateNickname();
+
+	});
+	
+});
